ae9e3a0b3a5f2f08d41020ece276e4406cc42992
Ignacio Pulido <ignacio.pulido236@gmail.com>
2023-03-28 13:28:51 -0400

    ok

--- blender/import_geometry.py
+++ blender/import_geometry.py
@@ -2,6 +2,11 @@ import bpy
 import bmesh
 import json
 import os
+
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete(use_global=False, confirm=False)
+
+
 # Create a new mesh data block
 mesh = bpy.data.meshes.new("Base")
 os.chdir(r"C:\Users\ignac\Upwork\Tom Hayden\json_files")
@@ -43,8 +48,22 @@ scene.collection.objects.link(obj)
 
 
 ########
+file_id=0
+file_id_len=len(str(file_id))
+if file_id_len==1:
+    selected_file='layer_0000' +str(file_id)+'.json'
+elif file_id_len==2:
+    selected_file='layer_000' +str(file_id)+'.json'
+elif file_id_len==3:
+    selected_file='layer_00' +str(file_id)+'.json'
+elif file_id_len==4:
+    selected_file='layer_0' +str(file_id)+'.json'
+elif file_id_len==5:
+    selected_file='layer_' +str(file_id)+'.json'
+    
+    
 os.chdir(r"C:\Users\ignac\Upwork\Tom Hayden\layers_info_json")
-with open('layer_06795.json', 'r') as f:
+with open(selected_file, 'r') as f:
     # Load the JSON data into a Python dictionary
     data = json.load(f)
 #print(data)
@@ -77,6 +96,9 @@ obj = bpy.data.objects.new(data['file_name'], mesh)
 # Add the object to the scene
 scene = bpy.context.scene
 scene.collection.objects.link(obj)
+
+
+#Save to stl
 
 filepath = "C:/Users/ignac/OneDrive/Escritorio/extruded.stl"
 bpy.ops.export_mesh.stl(filepath=filepath, check_existing=False)
